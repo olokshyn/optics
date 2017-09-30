d88e5eb2dad6cde8335d13825d85173a3e4e779b
Oleg Lokshyn <olokshyn@gmail.com>
2017-09-30 13:44:10 +0300

    Improved the way incorrectly clustered points are counted
    Earlier for a found cluster to be considered a match with a known cluster
    the first point of the found cluster should be clustered correctly,
    or the whole match is missed.
    
    Now for each known cluster the cluster that matches it best
    in terms of similar points is found, allowing for the
    match to be found even if the first point is clustered incorrectly.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def read_sample_input_data(data_filename):
     return points
 
 
-def read_class_labels(cls_filename):
+def read_cluster_labels(cls_filename):
     labels = []
     with open(cls_filename, 'r') as cls_file:
         for line in cls_file:
@@ -26,20 +26,39 @@ def read_class_labels(cls_filename):
     return labels
 
 
-def count_erroneously_clustered_points(clusters, noise, class_labels):
-    class_labels_converter = {}
+def count_erroneously_clustered_points(found_clusters, noise, cluster_labels):
+    # Reorganize cluster_labels so every label will denote
+    # a set of object`s indexes which it contains
+    clusters = {}
+    for index in xrange(len(cluster_labels)):
+        if cluster_labels[index] not in clusters:
+            clusters[cluster_labels[index]] = set()
+        clusters[cluster_labels[index]].add(index)
 
-    erroneously_clustered_points_count = len(noise)
-    for cluster_index, cluster in enumerate(clusters):
-        if cluster and cluster_index not in class_labels_converter:
-            class_labels_converter[cluster_index] = class_labels[cluster[0].index]
-        for obj in cluster:
-            if class_labels_converter[cluster_index] != class_labels[obj.index]:
-                erroneously_clustered_points_count += 1
+    # For every cluster find the biggest found_cluster that matches it
+    clusters_matched_count = {}
+    for cluster_label, cluster in clusters.iteritems():
+        clusters_matched_count[cluster_label] = 0
+
+        found_cluster_matches = {}
+        for found_cluster_index, found_cluster in enumerate(found_clusters):
+            found_cluster_matches[found_cluster_index] = 0
+            for obj in found_cluster:
+                if obj.index in cluster:
+                    found_cluster_matches[found_cluster_index] += 1
+        if not found_cluster_matches:
+            continue
+
+        clusters_matched_count[cluster_label] = max(found_cluster_matches.itervalues())
+
+    erroneously_clustered_points_count = 0
+    for cluster_label, matched_count in clusters_matched_count.iteritems():
+        erroneously_clustered_points_count += len(clusters[cluster_label]) - matched_count
+
     return erroneously_clustered_points_count
 
 
-def perform_clustering(data_filename, eps, min_pts, class_labels_filename='', build_plot=False):
+def perform_clustering(data_filename, eps, min_pts, cluster_labels_filename='', build_plot=False):
     points = read_sample_input_data(data_filename)
     cluster_ordering = optics(points, eps, min_pts)
     if build_plot:
@@ -50,11 +69,12 @@ def perform_clustering(data_filename, eps, min_pts, class_labels_filename='', bu
     clusters, noise = extract_clusters_and_noise(cluster_ordering, eps)
     print 'Clusters: {}, noise points: {}'.format(len(clusters), len(noise))
     erroneously_clustered_points_count = 0
-    if class_labels_filename:
-        class_labels = read_class_labels(class_labels_filename)
-        erroneously_clustered_points_count = count_erroneously_clustered_points(clusters, noise, class_labels)
-        print 'Erroneously clustered points: {}' \
-            .format(erroneously_clustered_points_count)
+    if cluster_labels_filename:
+        cluster_labels = read_cluster_labels(cluster_labels_filename)
+        erroneously_clustered_points_count = count_erroneously_clustered_points(clusters, noise, cluster_labels)
+        print 'Erroneously clustered points: {} ({}%)' \
+            .format(erroneously_clustered_points_count,
+                    float(erroneously_clustered_points_count) / len(cluster_labels) * 100)
     return clusters, noise, erroneously_clustered_points_count
 
 
@@ -65,31 +85,41 @@ def main(argv):
     step_eps = float(argv[4])
     min_min_pts = int(argv[5])
     max_min_pts = int(argv[6])
-    class_labels_filename = ''
+    cluster_labels_filename = ''
     if len(argv) >= 8:
-        class_labels_filename = argv[7]
+        cluster_labels_filename = argv[7]
 
     best_eps = 0.0
     best_min_pts = 0.0
     best_err_clust_points = None
+    best_err_clust_perc = 0.0
+    best_noise_count = 0
 
     eps = min_eps
     while eps <= max_eps:
         for min_pts in xrange(min_min_pts, max_min_pts + 1):
             print 'eps:', eps
             print 'min_pts:', min_pts
-            _, _, err_clust_points = perform_clustering(data_filename, eps, min_pts, class_labels_filename)
+            clusters, noise, err_clust_points = perform_clustering(data_filename, eps, min_pts, cluster_labels_filename)
             if best_err_clust_points is None or err_clust_points < best_err_clust_points:
                 best_eps = eps
                 best_min_pts = min_pts
                 best_err_clust_points = err_clust_points
+                best_err_clust_perc = float(best_err_clust_points) \
+                                      / (sum((len(x) for x in clusters)) + len(noise)) * 100
+                best_noise_count = len(noise)
             print ''
+            if best_err_clust_points == 0:
+                break
+        if best_err_clust_points == 0:
+            break
         eps += step_eps
 
     print ''
     print 'Best eps:', best_eps
     print 'Best min_pts:', best_min_pts
-    print 'Best erroneously clustered points:', best_err_clust_points
+    print 'Best erroneously clustered points: {} ({}%)'.format(best_err_clust_points, best_err_clust_perc)
+    print 'Best noise count:', best_noise_count
 
     return 0
 
